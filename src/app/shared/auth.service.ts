835b3519608a77dae8f6dfedaab02a1cd35e312f
gourish naik <gourish@campionsoftware.com>
2022-09-24 15:37:50 +0530

    done

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -11,9 +11,20 @@ export class AuthService {
 
   //login
   login(email:string,password:string){
-    this.fireauth.signInWithEmailAndPassword(email,password).then( ()=>{
+    this.fireauth.signInWithEmailAndPassword(email,password).then( (res)=>{
    localStorage.setItem('token','true');
-   this.router.navigate(['/dashboard']);
+  // this.router.navigate(['/dashboard']);
+
+//if user is not verified
+if(res.user?.emailVerified==true){
+ // this.sendEmailForVerification(res.user)
+  this.router.navigate(['/dashboard']);
+}
+else{
+  this.router.navigate(['/verify'])
+}
+
+
     },err=>{
 alert(err.message)
 this.router.navigate(['/login']);
@@ -22,9 +33,12 @@ this.router.navigate(['/login']);
 
   // register or signin
   register(email:string,password:string){
-    this.fireauth.createUserWithEmailAndPassword(email,password).then(()=>{
+    this.fireauth.createUserWithEmailAndPassword(email,password).then((res)=>{
       alert("registration is successful")
-      this.router.navigate(['/login'])
+      this.router.navigate(['/login']);
+      
+      //after registration send email for verification
+      this.sendEmailForVerification(res.user)
     },err=>{
       alert(err.message)
       this.router.navigate(['/register'])
@@ -41,5 +55,24 @@ logout(){
   })
 }
 
+
+//forgot password
+forgotpassword(email:string){
+this.fireauth.sendPasswordResetEmail(email).then(()=>{
+this.router.navigate(['/verify'])
+},err=>{
+    alert(err.message)
+  })
+}
+//registration verification
+sendEmailForVerification(user:any){
+//  firebase.auth().currentUser.()
+user.sendEmailVerification().then((res:any)=>{
+  console.log('email sent');
+   this.router.navigate(['/verify']);
+},(err:any)=>{
+  alert("something went wrong! try again")
+})
+}
 
 }
